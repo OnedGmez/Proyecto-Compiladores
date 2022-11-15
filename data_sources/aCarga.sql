b91ad901bb5fcf42434534699ee018ee3338bd1b
OnedGmez <oned.gomez@unah.hn>
2022-11-14 20:13:15 -0600

    Update

--- data_sources/aCarga.sql
+++ data_sources/aCarga.sql
@@ -1,29 +1,14 @@
 CREATE PROCEDURE RegistrarMetodoPago -holaaaa
 
-
-
 @CodMetodoPago VARCHAR(7), 
-
 *
 ESTO ES UN ERROR
 */
-
-
 @Descripcion VARCHAR(20),
-
-
 @Premiacion BIT
-
-
 AS
-
-
 BEGIN
-
-
-	INSERT INTO MetodosPago(CodMetodoPago, Descripcion, Premiacion) VALUES(@CodMetodoPago, @Descripcion, @Premiacion)
-
-
+SELECT * FROM MetodosPago
+INSERT INTO MetodosPago(CodMetodoPago, Descripcion, Premiacion) VALUES(@CodMetodoPago, @Descripcion, @Premiacion)
 END
-
 GO
