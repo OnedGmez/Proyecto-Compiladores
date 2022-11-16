303ea60430c5b4d6f46d1110c16132e8775718b4
OnedGmez <oned.gomez@unah.hn>
2022-11-15 21:59:32 -0600

    Archivo para colocar los códigos y realizar pruebas

--- data_sources/aCarga.sql
+++ data_sources/aCarga.sql
@@ -1,14 +1,39 @@
-CREATE PROCEDURE RegistrarMetodoPago -holaaaa
-
-@CodMetodoPago VARCHAR(7), 
-*
-ESTO ES UN ERROR
-*/
-@Descripcion VARCHAR(20),
-@Premiacion BIT
+CREATE FUNCTION ContraseniaRandom 
+()
+RETURNS VARCHAR(10)
 AS
-BEGIN
-SELECT * FROM MetodosPago
-INSERT INTO MetodosPago(CodMetodoPago, Descripcion, Premiacion) VALUES(@CodMetodoPago, @Descripcion, @Premiacion)
+BEGIN    
+
+/*
+ESTO ES UN COMENTARIO MULTIPLE CONN ERROR
+*
+
+    DECLARE @chars AS VARCHAR(52),
+            @numbers AS VARCHAR(10),
+            @Caracteres AS VARCHAR(62),        
+            @contrasenia AS VARCHAR(62),
+            @index AS INT,
+            @cont AS INT
+
+    SET @contrasenia = ''
+    SET @Caracteres = ''    
+    SET @chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    SET @numbers = '0123456789'
+
+    SET @Caracteres = @chars + @numbers
+
+    SET @cont = 0
+    WHILE @cont < 20
+    BEGIN
+        SET @index = ceiling( ( SELECT rnd FROM RandomNumero ) * (len(@Caracteres)))
+        SET @contrasenia = @contrasenia + substring(@Caracteres, @index, 1)
+        SET @cont = @cont + 1
+    END    
+    *
+    ESTO ES UN COMENTARIO MULTIPLE CON ERROR
+    */
+        
+    RETURN @contrasenia
+
 END
 GO
